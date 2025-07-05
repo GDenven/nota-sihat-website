4438149ae3ca33743bb8ff4644aeddfcaa9ed7ae
GDenven <gdenven2@gmail.com>
2025-07-05 17:56:38 +0800

    add new content

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 //get the current year and displaying it in the footer
-var currentYear = new Date().getFullYear();
-  
+var currentYear = new Date().getFullYear(); 
 document.getElementById("current-year").innerHTML = currentYear;
+
+//Trigger google search into search bar
+const searchForm = document.querySelector('.search-form');
+
+searchForm.addEventListener('submit', (event) => {
+  // Prevent the default form submission behavior (which would reload the page)
+  event.preventDefault();
+
+  const searchInput = document.querySelector('.search-input');
+  const searchTerm = searchInput.value;
+
+  // Construct the Google search URL
+  const googleSearchURL = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+
+  // Open the Google search results in a new tab/window
+  window.open(googleSearchURL, '_blank');
+});
+
